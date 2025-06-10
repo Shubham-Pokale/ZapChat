66bfc03a39bfe9c2dd2b4db685d3f492d7429699
Shubham-Pokale <shubham.pokale@appzlogic.com>
2025-06-10 12:13:07 +0530

    updated the user model with audit fields

---
+++ app/db/models/__init__.py
@@ -0,0 +1,4 @@
+from .user import User
+from .chat_room import ChatRoom
+from .room_participant import RoomParticipant
+from .chat_message import ChatMessage
--- app/db/models/user.py
+++ app/db/models/user.py
@@ -1,13 +1,17 @@
-from sqlalchemy import Boolean,Column, Integer,String
+from sqlalchemy import Boolean, Column, Integer, String, DateTime
 from app.db.base_class import Base
 from sqlalchemy.orm import declarative_base
+from datetime import datetime
 
-class User(Base) : 
+class User(Base):
     __tablename__ = "users"
     
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
+    username = Column(String(50), unique=True, index=True, nullable=False)
+    email = Column(String(100), unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
-    full_name = Column(String, index=True)
     is_active = Column(Boolean(), default=True)
-    is_superuser = Column(Boolean(), default=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    # Relationships
+    participant_rooms = relationship("RoomParticipant", back_populates="user")
+    sent_messages = relationship("ChatMessage", back_populates="sender")

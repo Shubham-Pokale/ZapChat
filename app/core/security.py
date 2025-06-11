e2f865fce129ddfcd4483c3a5495353089cf1e07
Shubham-Pokale <shubham.pokale@appzlogic.com>
2025-06-11 11:45:28 +0530

    rectifying jwt token auth

--- app/core/security.py
+++ app/core/security.py
@@ -4,6 +4,7 @@ from jose import jwt, JWTError
 from pydantic import ValidationError
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
+from datetime import datetime, timedelta
 
 from app.db.models.user import User
 from app.schemas.user import User as UserSchema
@@ -22,6 +23,19 @@ credentials_exception = HTTPException(
 
 def get_password_hash(password: str):
     return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
 
 def get_current_user(
     db: Session = Depends(get_db),
